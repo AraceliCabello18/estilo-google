5cba64926563732b4fb4bb21b795241e0a520fd4
AraceliCabello18 <aylincabello786@gmail.com>
2021-03-24 18:36:58 -0600

    IMAGENES ETILO GOOGLE

---
+++ contenidos.php
@@ -0,0 +1,52 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/Vampire Knight 16 de Panini Comics Manga.png"."||".
+				  "Vampire Knight"."||".
+				  "El primer recuerdo de la vida de Yuuki es una noche de tormenta en invierno,
+          donde fue atacada por un vampiro y luego rescatada por otro. Diez años después, 
+          se ha convertido en guardiana de la academia Cross, donde también acuden vampiros. 
+          Allí protege al chico que le gusta, Kaname. Yuuki descubre que él casualmente es el líder 
+          de la comunidad vampírica en el colegio. A su lado en la tarea está su amigo de 
+          la infancia Kiryuu Zero, que odia a todo aquello relacionado con los vampiros. 
+          Todo está en paz hasta que una fuerza oscura amenaza con destruir la perfecta coexistencia.";
+
+		$datos[1]="img/el falso matrimonio.jpg"."||".
+				  "Este es un obvio matrimonio fraudulento"."||".
+				  "El país de Sophen había sido destruido por el Imperio. La única hija de una familia de condes, Leylin Efran, 
+          tuvo que encontrar un marido para poder heredar a su familia. Para no tener que sufrir por un matrimonio sin amor,
+          Leylin decidió buscar un marido con quien tener un contrato de matrimonio de un año. En medio de su búsqueda, un joven
+          y apuesto hombre apareció ante ella. Leylin estaba convencida de que este es el hombre adecuado para ella, pero ¿es realmente
+          la respuesta a la peligrosa situación de Leylin?";
+
+		$datos[2]="img/Gakuen_Alice.jpg"."||".
+				  "Gakuen Alice"."||".
+				  "Gakuen Alice cuenta la historia de Mikan Sakura, una niña de 10 años que en busca de su mejor amiga, 
+          Imai Hotaru, ingresa de forma imprevista en una escuela muy especial y extraña. A ella sólo pueden entrar
+          personas que posean un alice, es decir un poder especial que puede ser de cualquier tipo, desde la telepatía 
+          o volar hasta controlar el fuego o convertirse en gato. Y Mikan sin saberlo cuenta con un alice,
+          que al principio para todos era insignificante por eso termino en grupo de los alice especiales. 
+          Tambien hay un grupo de alice peligros, en el cual se encuentra Natsume, los estudiantes de este
+          grupos son utilizados por los directivos del colegio para realizar misiones secretas y peligrosas.";
+
+		$datos[3]="img/akatsuki no yona.png"."||".
+				  "Akatsuki no Yona"."||".
+				  "Yona es la única princesa del reino Kouka, viviendo la vida lujosa y despreocupada como una princesa debería.
+          Ella lo tiene todo: las mejores ropas, cosméticos, los dulces más divinos, un emperador amoroso como su padre, 
+          y a su guapo primo, Soo-won, y su amigo de la infancia y guardaespaldas, Hak. Pero un evento cruel e inesperado 
+          ocurre que va a cambiar el curso de su destino.";
+
+		$datos[4]="img/Lucia.jpg"."||".
+				  "Lucia"."||".
+				  "Lucía creció sin saber que era ser una princesa. Pero cuando su Madre murió,
+          ella entró en el palacio y tuvo la oportunidad de ver todo su futuro en un sueño.
+          En su sueño, cuando tenía 19 años, la subastaron al licitador que ofreció la dote más alta.
+          Su vida se volvió miserable a partir de ese momento. Cuando se despertó de su sueño, estaba 
+          decidida a cambiar su futuro, al darse cuenta de que tenía mucho tiempo antes del momento crucial. 
+          Así, comienza su viaje para cambiar su oscuro futuro.";
+		return $datos;
+	}
+
+ ?>
